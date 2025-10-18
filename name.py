a6b8eabeb42a43acad20d0e8251daae95d6cac44
Vadim-Codland <vadimgloba12@mail.ru>
2025-10-18 11:48:49 +0300

    Update name.py

--- name.py
+++ name.py
@@ -1,11 +1,37 @@
-meme_dict = {
-            "КРИНЖ": "Что-то очень странное или стыдное",
-            "ЛОЛ": "Что-то очень смешное"
-            }
+import requests
+import q
 
-word = input("Введите непонятное слово (большими буквами!): ")
-if word in meme_dict.keys():
-    print (meme_dict [word])
-    # Что делать, если слово нашлось?
-else:
-    print ("такого слова нет")
+
+def gpt(text):
+    prompt = {
+        "modelUri": f"gpt://{q.id_ya}/yandexgpt",
+        "completionOptions": {
+            "stream": False,
+            "temperature": 0.6,
+            "maxTokens": "4000"
+        },
+        "messages": [
+            {
+                'role':'system',
+                'text': '''
+ты очень переживаешь за чистоту природы рассказываешь про важность чистоты природы , можешь рассказать
+о том как утилизировать вещества и предметы и сколько что будет разлогаться но только тогда когда тебя 
+об этом просят и ты умеешь шутить'''
+            },
+            {
+                "role": "user",
+                "text": text
+            }
+        ]
+    }
+    
+    
+    url = "https://llm.api.cloud.yandex.net/foundationModels/v1/completion"
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Api-Key {q.key_ya}"
+    }
+    
+    response = requests.post(url, headers=headers, json=prompt)
+    result = response.json().get('result')
+    return result['alternatives'][0]['message']['text']
